73c01a2dc666c39bf3ea48a9a775b4492d608b0c
Nano Galvão <messivanio.galvao@gmail.com>
2014-04-27 13:31:59 -0300

    Transposes a progression with inverted chords.
    Chord.transpose(['C/E','F/A','Am/G','G/B']).to('D').should ==
    ['D/F#','G/B','Bm/A','A/C#']

--- spec/lib/transpose_chords_spec.rb
+++ spec/lib/transpose_chords_spec.rb
@@ -7,12 +7,20 @@ module TransposeChords
         Chord.transpose(['C','F','Am','G']).to('D').should == ['D','G','Bm','A']
       end
 
+      it 'transposes a chord progression with inverted chords' do
+        Chord.transpose(['C/E','F/A','Am/G','G/B']).to('D').should == ['D/F#','G/B','Bm/A','A/C#']
+      end
+
       it 'transposes a chord progression to a sharp note' do
         Chord.transpose(['C','F','Am','G']).to('D#').should == ['D#','G#','Cm','A#']
       end
 
       it 'transposes a chord progression to a flat note' do
         Chord.transpose(['Cm','F','Am','G']).to('Bb').should == ['Bbm','Eb','Gm','F']
+      end
+
+      it 'transposes a chord progression to a flat note scale (F)' do
+        Chord.transpose(['C','F','Am','G']).to('F').should == ['F','Bb','Dm','C']
       end
     end
 
