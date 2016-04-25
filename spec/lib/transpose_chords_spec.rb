2ab44cfc53c54bb6c106b2053f552fb9cdc88a5d
Richard Patching <richard@justaddpixels.com>
2016-04-25 15:14:44 +0100

    Update dependencies

--- spec/lib/transpose_chords_spec.rb
+++ spec/lib/transpose_chords_spec.rb
@@ -4,29 +4,29 @@ module TransposeChords
   describe Chord do
     context 'transposing to a new key' do
       it 'transposes a chord progression' do
-        Chord.transpose(['C','F','Am','G']).to('D').should == ['D','G','Bm','A']
+        expect(Chord.transpose(['C','F','Am','G']).to('D')).to match_array(['D','G','Bm','A'])
       end
 
       it 'transposes a chord progression with inverted chords' do
-        Chord.transpose(['C/E','F/A','Am/G','G/B']).to('D').should == ['D/F#','G/B','Bm/A','A/C#']
+        expect(Chord.transpose(['C/E','F/A','Am/G','G/B']).to('D')).to match_array(['D/F#','G/B','Bm/A','A/C#'])
       end
 
       it 'transposes a chord progression to a sharp note' do
-        Chord.transpose(['C','F','Am','G']).to('D#').should == ['D#','G#','Cm','A#']
+        expect(Chord.transpose(['C','F','Am','G']).to('D#')).to match_array(['D#','G#','Cm','A#'])
       end
 
       it 'transposes a chord progression to a flat note' do
-        Chord.transpose(['Cm','F','Am','G']).to('Bb').should == ['Bbm','Eb','Gm','F']
+        expect(Chord.transpose(['Cm','F','Am','G']).to('Bb')).to match_array(['Bbm','Eb','Gm','F'])
       end
 
       it 'transposes a chord progression to a flat note scale (F)' do
-        Chord.transpose(['C','F','Am','G']).to('F').should == ['F','Bb','Dm','C']
+        expect(Chord.transpose(['C','F','Am','G']).to('F')).to match_array(['F','Bb','Dm','C'])
       end
     end
 
     context 'transposing with a capo' do
       it 'transposes the chords' do
-        Chord.transpose(['C','D','G']).capo(2).should == ['D','E','A']
+        expect(Chord.transpose(['C','D','G']).capo(2)).to match_array(['D','E','A'])
       end
     end
   end
